8878566cedc779fd829f9e7fce8d9125a8d8ec01
emarcasdev <emarcasdev@gmail.com>
2025-02-25 19:00:10 +0100

    volver como al princio del login.js *_*

--- scripts/login.js
+++ scripts/login.js
@@ -10,11 +10,12 @@ $(document).ready(function () {
       let password = $(".password-field").val();
   
       $.ajax({
-        type: "POST", 
+        type: "POST",
         url: uri,
         contentType: "application/json",
         dataType: "json",
-        data: JSON.stringify({ username, password }), 
+        processData: false,
+        data: JSON.stringify({ username, password }),
         success: function (response) {
           if (response.success) {
             let messageAccess = `<div class="card">
@@ -33,8 +34,9 @@ $(document).ready(function () {
             setTimeout(() => {
               window.location.href = "../pages/index.html";
             }, 500);
-          } else {
-            $(".password-field").val("");
+            
+          } else if (response.message === "Incorrect password") {
+            $(".password-field").val("");    
             let messageError = `<div class="card">
                       <div class="icon-container">
                         <i class="bx bxs-error icon"></i>
@@ -48,26 +50,42 @@ $(document).ready(function () {
                       <i class="bx bx-x cross-icon"></i>
                   </div>`;
             $("#error-message").append(messageError);
+          } else {
+            $(".username-field").val("");
+            $(".password-field").val("");    
+            let messageError = `<div class="card">
+                      <div class="icon-container">
+                        <i class="bx bxs-error icon"></i>
+                      </div>
+                    
+                      <div class="message-text-container">
+                        <p class="message-text">Error</p>
+                        <p class="sub-text">${response.message}</p>
+                      </div>
+                    
+                      <i class="bx bx-x cross-icon"></i>
+                  </div>`;
+            $("#error-message").append(messageError);
           }
         },
         error: function (error) {
           $(".username-field").val("");
-          $(".password-field").val("");
-          let messageError = `<div class="card">
-                    <div class="icon-container">
-                      <i class="bx bxs-error icon"></i>
-                    </div>
-                  
-                    <div class="message-text-container">
-                      <p class="message-text">Error</p>
-                      <p class="sub-text">Error al conectarse a la base de datos.</p>
-                    </div>
-                  
-                    <i class="bx bx-x cross-icon"></i>
-                </div>`;
-          $("#error-message").append(messageError);
+            $(".password-field").val("");    
+            let messageError = `<div class="card">
+                      <div class="icon-container">
+                        <i class="bx bxs-error icon"></i>
+                      </div>
+                    
+                      <div class="message-text-container">
+                        <p class="message-text">Error</p>
+                        <p class="sub-text">Error al conectarse a la base de datos.</p>
+                      </div>
+                    
+                      <i class="bx bx-x cross-icon"></i>
+                  </div>`;
+            $("#error-message").append(messageError);
         },
-      });      
+      });
     });
   
     $("#error-message").on("click", ".cross-icon", function () {
